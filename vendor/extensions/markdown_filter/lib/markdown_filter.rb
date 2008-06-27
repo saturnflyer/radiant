c9acbfa8d8964f3b6cdc377a5fc7b625eb22e919
johnmuhl <git@johnmuhl.com>
2008-06-27 16:55:25 -0600

    Fix problem with overriding BlueCloth causing an "already initialized constant" warning

--- vendor/extensions/markdown_filter/lib/markdown_filter.rb
+++ vendor/extensions/markdown_filter/lib/markdown_filter.rb
@@ -1,14 +1,15 @@
 begin
   require 'rdiscount'
-  BlueCloth = RDiscount
+  YourCloth = RDiscount
   rescue LoadError
     require 'bluecloth'
+    YourCloth = BlueCloth
   end
 require 'rubypants/rubypants'
 
 class MarkdownFilter < TextFilter
   description_file File.dirname(__FILE__) + "/../markdown.html"
   def filter(text)
-    RubyPants.new(BlueCloth.new(text).to_html).to_html
+    RubyPants.new(YourCloth.new(text).to_html).to_html
   end
 end
