b84da090e105008de00443f54e478ab59fa7d2c7
johnmuhl <git@johnmuhl.com>
2008-06-28 12:59:15 -0600

    * add a matcher that makes dependencies spec-able * add specs for Markdown/SmaryPants dependencies

--- vendor/extensions/markdown_filter/spec/models/markdown_filter_spec.rb
+++ vendor/extensions/markdown_filter/spec/models/markdown_filter_spec.rb
@@ -1,6 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe MarkdownFilter do
+  it "depends on a Markdown processing library" do
+    either_of(:rdiscount, :bluecloth).should be_loadable
+  end
+
   it "should be named Markdown" do
     MarkdownFilter.filter_name.should == "Markdown"
   end
