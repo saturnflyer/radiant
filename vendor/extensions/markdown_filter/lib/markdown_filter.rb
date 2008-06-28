6a2e83d542c76f29afca9c49584eba96a73f5cd9
johnmuhl <git@johnmuhl.com>
2008-06-28 13:10:59 -0600

    Markdown seems like a more reasonable name than YourCloth

--- vendor/extensions/markdown_filter/lib/markdown_filter.rb
+++ vendor/extensions/markdown_filter/lib/markdown_filter.rb
@@ -1,15 +1,15 @@
 begin
   require 'rdiscount'
-  YourCloth = RDiscount
+  Markdown = RDiscount
   rescue LoadError
     require 'bluecloth'
-    YourCloth = BlueCloth
+    Markdown = BlueCloth
   end
 require 'rubypants/rubypants'
 
 class MarkdownFilter < TextFilter
   description_file File.dirname(__FILE__) + "/../markdown.html"
   def filter(text)
-    RubyPants.new(YourCloth.new(text).to_html).to_html
+    RubyPants.new(Markdown.new(text).to_html).to_html
   end
 end
