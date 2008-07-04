cfb1169c3f10143f0002e678d9faf241df270668
Jim Gay <jim@saturnflyer.com>
2008-07-04 14:31:02 -0400

    clarifying page_part_spec

--- spec/models/page_part_spec.rb
+++ spec/models/page_part_spec.rb
@@ -3,32 +3,63 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe PagePart do
   test_helper :page_parts, :validations
   
-  before :all do
+  before :each do
     @part = @model = PagePart.new(PagePartTestHelper::VALID_PAGE_PART_PARAMS)
   end
   
-  it 'should validate length of' do
-    {
-      :name => 100,
-      :filter_id => 25
-    }.each do |field, max|
-      assert_invalid field, ('%d-character limit' % max), 'x' * (max + 1)
-      assert_valid field, 'x' * max
-    end
+  it 'should not save with a name longer than 100 characters' do
+    too_long_name = 'x' * 101
+    @part.name = too_long_name
+    lambda {
+      @part.save!
+    }.should raise_error(ActiveRecord::RecordInvalid)
   end
   
-  it 'should validate presence of' do
-    [:name].each do |field|
-      assert_invalid field, 'required', '', ' ', nil
-    end
+  it 'should not save with a filter_id longer than 25 characters' do
+    too_long_filter_id = 'x' * 26
+    @part.filter_id = too_long_filter_id
+    lambda {
+      @part.save!
+    }.should raise_error(ActiveRecord::RecordInvalid)
   end
   
-  it 'should validate numericality of' do
-    [:id, :page_id].each do |field|
-      assert_valid field, '1', '2'
-      assert_invalid field, 'must be a number', 'abcd', '1,2', '1.3'
-    end
+  it 'should save with no filter_id' do
+    @part.filter_id = nil
+    lambda{ @part.save! }.should_not raise_error
   end
+  
+  it 'should not save without a name' do
+    @part.name = nil
+    lambda {
+      @part.save!
+    }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it 'should not save if page_id is not an integer' do
+    @part.page_id = '1.3'
+    lambda {
+      @part.save!
+    }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it 'should not save if id is not an integer' do
+    @part.id = '1.3'
+    lambda {
+      @part.save!
+    }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it 'should save with no page_id' do
+    @part.page_id = nil
+    lambda {@part.save!}.should_not raise_error
+  end
+  
+  it 'should save with no id' do
+    @part.id = nil
+    lambda {@part.save!}.should_not raise_error
+  end
+
+  it 'should find parts ordered by name'
 end
 
 describe PagePart, 'filter' do
