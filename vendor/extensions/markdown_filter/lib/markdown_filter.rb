86156c328d06b3e99a1c78ea89ab418d2b1c8a81
johnmuhl <git@johnmuhl.com>
2008-07-18 19:40:41 -0600

    that shouldn't be in the spec branch

--- vendor/extensions/markdown_filter/lib/markdown_filter.rb
+++ vendor/extensions/markdown_filter/lib/markdown_filter.rb
@@ -1,21 +1,9 @@
-begin
-  gem 'rdiscount', '>= 1.2.6'
-  require 'rdiscount'
-  Markdown = RDiscount
-  class MarkdownFilter < TextFilter
-    description_file File.dirname(__FILE__) + "/../markdown.html"
-    def filter(text)
-      Markdown.new(text, :smart).to_html
-    end
+require 'bluecloth'
+require 'rubypants/rubypants'
+
+class MarkdownFilter < TextFilter
+  description_file File.dirname(__FILE__) + "/../markdown.html"
+  def filter(text)
+    RubyPants.new(BlueCloth.new(text).to_html).to_html
   end
-  rescue LoadError
-    require 'bluecloth'
-    require 'rubypants/rubypants'
-    Markdown = BlueCloth
-    class MarkdownFilter < TextFilter
-      description_file File.dirname(__FILE__) + "/../markdown.html"
-      def filter(text)
-        RubyPants.new(Markdown.new(text).to_html).to_html
-      end
-    end
-  end
+end
