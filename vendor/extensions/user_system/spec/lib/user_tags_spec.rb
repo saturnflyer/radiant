15ce0f2c4673742196b685abedc96493d7777fea
Jim Gay <jim@saturnflyer.com>
2010-04-11 22:27:41 -0400

    move user management into user_system extension
    This allows developers to do
      config.extensions = [:my_replacement_user_system, :all]
      canfig.extensions -= [:user_system]
    which will allow a completely custom login and user
    management solution. Currently, the bootstrap process
    still relies on the existance of a User model. This
    should be changed to allow some altered bootstrap process
    such as loading seed data or templates from extensions.
    
    An additional caveat is that the migrations are still in
    the main project's migrations rather than the extension's.
    A separate bit of code will be required to address this.

---
+++ vendor/extensions/user_system/spec/lib/user_tags_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "User Tags" do
+  dataset :users_and_pages
+
+  describe "<r:author>" do
+    it "should render the author of the current page" do
+      pages(:home).should render('<r:author />').as('Admin')
+    end
+
+    it "should render nothing when the page has no author" do
+      pages(:no_user).should render('<r:author />').as('')
+    end
+  end
+
+  describe "<r:gravatar>" do
+    it "should render the Gravatar URL of author of the current page" do
+      page.should render('<r:gravatar />').as('http://www.gravatar.com/avatar.php?gravatar_id=e64c7d89f26bd1972efa854d13d7dd61&rating=G&size=32')
+    end
+
+    it "should render the Gravatar URL of the name user" do
+      page.should render('<r:gravatar name="Admin" />').as('http://www.gravatar.com/avatar.php?gravatar_id=e64c7d89f26bd1972efa854d13d7dd61&rating=G&size=32')
+    end
+
+    it "should render the default avatar when the user has not set an email address" do
+      page.should render('<r:gravatar name="Designer" />').as('http://testhost.tld/images/admin/avatar_32x32.png')
+    end
+
+    it "should render the specified size" do
+      page.should render('<r:gravatar name="Designer" size="96px" />').as('http://testhost.tld/images/admin/avatar_96x96.png')
+    end
+
+    it "should render the specified rating" do
+      page.should render('<r:gravatar rating="X" />').as('http://www.gravatar.com/avatar.php?gravatar_id=e64c7d89f26bd1972efa854d13d7dd61&rating=X&size=32')
+    end
+  end
+    
+end
