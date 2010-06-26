0f6098b1e8e0b34b3c1a5bf9edc60ffd29e24a39
Jim Gay <jim@saturnflyer.com>
2010-06-26 10:35:04 -0400

    move admin ui spec into the extension manager

---
+++ vendor/extensions/extension_manager/spec/lib/admin_ui_spec.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe Radiant::AdminUI do
+  before :each do
+    @admin = Radiant::AdminUI.new
+  end
+  
+  it "should load the default extension regions" do
+    ext = @admin.extension
+    ext.index.should_not be_nil
+    ext.index.thead.should == %w{title_header website_header version_header}
+    ext.index.tbody.should == %w{title_cell website_cell version_cell}
+  end
+end
