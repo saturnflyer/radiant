d282d4f104169665d86ec25262f179d430bb6e34
Jim Gay <jim@saturnflyer.com>
2010-06-26 14:30:17 -0400

    load the admin ui instance instead of new

--- vendor/extensions/extension_manager/spec/lib/admin_ui_spec.rb
+++ vendor/extensions/extension_manager/spec/lib/admin_ui_spec.rb
@@ -1,12 +1,12 @@
-require File.dirname(__FILE__) + "/../../spec_helper"
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Radiant::AdminUI do
   before :each do
-    @admin = Radiant::AdminUI.new
+    @admin = Radiant::AdminUI.instance
   end
   
   it "should load the default extension regions" do
-    ext = @admin.extension
+    ext = @admin.extensions
     ext.index.should_not be_nil
     ext.index.thead.should == %w{title_header website_header version_header}
     ext.index.tbody.should == %w{title_cell website_cell version_cell}
